8169bc608d6f88c7963ffff430e9791619b40237
K <137853302@qq.com>
2022-01-19 22:43:21 +0800

    使用Feign替换RestTemplate,保持URL请求简单，方便

---
+++ simple-consumer-movie-feign/src/main/java/com/example/simpleconsumermoviefeign/SimpleConsumerMovieFeignApplication.java
@@ -0,0 +1,21 @@
+package com.example.simpleconsumermoviefeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableFeignClients
+@SpringBootApplication
+public class SimpleConsumerMovieFeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleConsumerMovieFeignApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
